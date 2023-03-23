6ef32623edb6f4d41946ec396e7076c981f1bc6e
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-03-23 14:28:32 +0100

    send

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,29 @@ import { Provider } from "react-redux"
 import { persistor, store } from "../app/store"
 import withRedux from 'next-redux-wrapper';
 import { PersistGate } from 'redux-persist/integration/react';
-import {SessionProvider} from 'next-auth/react'
-import {useRouter} from "next/router"
-import {useState, useEffect} from "react"
+import { SessionProvider } from 'next-auth/react'
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 
 function Loadind() {
-  
+
 }
 
 function MyApp({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+
+          <Component {...pageProps} />
+        </QueryClientProvider>
+
       </PersistGate>
     </SessionProvider>
-
   )
 }
 
